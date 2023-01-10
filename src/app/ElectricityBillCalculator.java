90f0da17a613486a68a48fee89d5efc347284406
SHyeonCoding <tkdgus3365@gmail.com>
2023-01-10 22:21:57 +0900

    Refactoring

--- src/app/ElectricityBillCalculator.java
+++ src/app/ElectricityBillCalculator.java
@@ -1,33 +1,24 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ElectricityBillCalculator {
     private int powerUsage = InputPowerUsage.powerUsage();
     private double electricityBill = 0;
     // 주택용 전기요금(저압)
-    private double[] houseElectricityBills = {60.7, 125.9, 187.9, 280.6, 417.7, 670.6};
+    private List<Double> houseElectricityBills = new ArrayList<>(Arrays.asList(60.7, 125.9, 187.9, 280.6, 417.7, 670.6));
 
     public double billCalculator() {
         double remainder = powerUsage % 100;
-        switch (powerUsage / 100) {
-            case 0:
-                electricityBill = houseElectricityBills[0] * remainder;
-                return electricityBill;
-            case 1:
-                electricityBill = houseElectricityBills[0] * 100 + houseElectricityBills[1] * remainder;
-                return electricityBill;
-            case 2:
-                electricityBill = houseElectricityBills[0] * 100 + houseElectricityBills[1] * 100 + houseElectricityBills[2] * remainder;
-                return electricityBill;
-            case 3:
-                electricityBill = houseElectricityBills[0] * 100 + houseElectricityBills[1] * 100 + houseElectricityBills[2] * 100 + houseElectricityBills[3] * remainder;
-                return electricityBill;
-            case 4:
-                electricityBill = houseElectricityBills[0] * 100 + houseElectricityBills[1] * 100 + houseElectricityBills[2] * 100 + houseElectricityBills[3] * 100 + houseElectricityBills[4] * remainder;
-                return electricityBill;
-            default:
-                electricityBill = houseElectricityBills[0] * 100 + houseElectricityBills[1] * 100 + houseElectricityBills[2] * 100 + houseElectricityBills[3] * 100 + houseElectricityBills[4] * 100 + houseElectricityBills[5] * (powerUsage - 500);
-                return electricityBill;
+        int value = powerUsage / 100;
+
+        for(int i = 0; i < value; i++) {
+            electricityBill += houseElectricityBills.get(i) * 100;
         }
+        electricityBill = electricityBill + remainder * houseElectricityBills.get(value);
+        return electricityBill;
     }
 }
 
--- src/app/InputPowerUsage.java
+++ src/app/InputPowerUsage.java
@@ -3,12 +3,10 @@ package app;
 import java.util.Scanner;
 
 public class InputPowerUsage {
-    private static int power;
 
     public static int powerUsage() {
         System.out.print("전력 사용량을 입력해주세요 : ");
         Scanner scanner = new Scanner(System.in);
-        power = Integer.parseInt(scanner.nextLine());
-        return power;
+        return Integer.parseInt(scanner.nextLine());
     }
 }
